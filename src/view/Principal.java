2261433224ea4b4788154e16a6f8e9a7e15ab9ec
Carol <Carol@DESKTOP-9ILSHFO>
2023-04-26 00:30:16 -0300

    commit

---
+++ src/view/Principal.java
@@ -0,0 +1,20 @@
+package view;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int [] vetor= {69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1};
+		
+		br.com.arccarol.QuickSort.Ordenacao ordenacao= new br.com.arccarol.QuickSort.Ordenacao();
+		
+		vetor=ordenacao.quickSort(vetor, 0, vetor.length -1);
+		
+		
+		for (int valor: vetor ) {
+			System.out.println(valor);
+			System.out.println(" ");
+		}
+	}
+	
+
+	}
